3b4d49e008fbc364b5ab35570c0c8b8fc6e2cc5f
Louis Horton <lhorton@anthropologie.com>
2015-04-02 12:46:57 -0700

    Nav system nearly complete, localstorage used

--- scripts/mapEngine.js
+++ scripts/mapEngine.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
-	navigator.geolocation.getCurrentPosition(geoSuccess, geoError, {maximumAge: 20000, timeout: 10000, enableHighAccuracy: true});
-});
+var buildingData = [];
+var lastSelectedMenu = -1;
+
+var autoGeolocation = function() {
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError, {maximumAge: 20000, timeout: 10000, enableHighAccuracy: true});
+}
 
 var geoSuccess = function (position) {
-	console.log(position.coords.latitude,position.coords.longitude);
 	$('#lat').append(position.coords.latitude);
 	$('#lng').append(position.coords.longitude);
 }
@@ -11,4 +13,43 @@ var geoSuccess = function (position) {
 var geoError = function (position) {
 	console.log('geolocation error');
 }
+
+var readMenu = function () {
+  $.ajax({
+    dataType: "json",
+    url: "json/data.json",
+    success: function (data) {  
+      buildingData = data;
+      lastSelectedMenu = localStorage.getItem("URBN-Building") > -1 ? localStorage.getItem("URBN-Building") : 0;
+
+      $.each(buildingData, function(index, element) {
+        var mainMenu = $('.monster-main-menu');
+        var breadCrumb = $('.breadcrumb');
+
+        mainMenu.append("<li onclick='loadBuildingDetails(" + index + ")'>" + buildingData[index].name + "</li>");
+        
+        //Initial breadcrumb setup
+        breadCrumb.empty();
+        breadCrumb.append("<li>" + buildingData[lastSelectedMenu].name + "</li>");
+      });
+    }
+  });
+}
+
+var loadBuildingDetails = function(buildingId) {
+  var breadCrumb = $('.breadcrumb');
+
+  breadCrumb.empty();
+  breadCrumb.append("<li>" + buildingData[buildingId].name + "</li>");
+  localStorage.setItem("URBN-Building", buildingId);
+}
+
+$(document).ready(function() {
+
+  //Read in the location menu options
+  readMenu();
+
+  //Detect up the geolocation
+  autoGeolocation();
+});
   	
